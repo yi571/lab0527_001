e1d192e54b9876052ecb247cecad7bcea54fa0eb
yi571 <yi571@outlook.com>
2018-05-27 16:14:41 +0800

    Initial commit

---
+++ lab0527_001/CAddScore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab0527_001 {
+    public class CAddScore {
+        public int scoreID { get; set; }
+        public string name { get; set; }
+        public string costTime { get; set; }
+    }
+}
---
+++ lab0527_001/addSuccess.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lab0527_001 {
+    public partial class addSuccess : System.Web.UI.Page {
+
+        guessNumDBEntities db = new guessNumDBEntities();
+
+        
+
+        protected void Page_Load(object sender, EventArgs e) {
+            var query = from o in db.Scores
+                        select o;
+            Score cas = new Score() {
+                name = Request["name"].ToString(),
+                costTime = Request["costTime"].ToString()
+            };
+            db.Scores.Add(cas);
+
+            db.SaveChanges();
+            
+        }
+    }
+}
